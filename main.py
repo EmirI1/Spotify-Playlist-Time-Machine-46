bbf3f72ae00dd96fa2b38a00b09a978cdb2f2a29
emir <emiribric10@gmail.com>
2025-03-03 21:58:17 -0500

    Get list of songs from top 100 from YYYY-MM-DD. Get access to spotify account via API to make and manage playlist

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from dotenv import load_dotenv, dotenv_values
-import os
 from bs4 import BeautifulSoup
 import requests
 import spotipy
@@ -9,17 +7,22 @@ user_input = input("What year do you want to travel to? Type the date in this fo
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 base_url = "https://www.billboard.com/charts/hot-100/"
 
-client_id = os.getenv("CLEINT_ID_KEY")
-client_secret = os.getenv("CLEINT_SECRET_KEY")
 
 response = requests.get(url=f"{base_url}{user_input}/", headers=header)
 response.raise_for_status
 html_webpage = response.text
 
-
 soup = BeautifulSoup(html_webpage, "html.parser")
 web_scraped_song_names = soup.select("li ul li h3")
 
-
 lst_of_song_names = [song.getText().strip() for song in web_scraped_song_names]
-print(lst_of_song_names)
+
+# Using spotipy for authenticating to the user 
+
+scope = "user-library-read"
+
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+results = sp.current_user_saved_tracks()
+print(results)
